834c964add7297113d81222acbbb31a364a7ad5d
shaoyansong01 <shaoyansong815@163.com>
2020-07-16 16:19:15 +0800

    second commit

---
+++ src/main/java/cn/kgc/controller/TestController.java
@@ -0,0 +1,13 @@
+package cn.kgc.controller;
+
+/**
+ * @author 邵岩松
+ * @create 2020-07-16
+ * 类描述：
+ */
+
+public class TestController {
+    public static void main (String[] args){
+        System.out.println("ok,ok,ok");
+    }
+}
